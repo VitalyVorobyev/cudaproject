3eb1d461e934a71310dad0c9691aa64fd4c52060
Vitaly Vorobyev <vitaly.vorobyev@quiss.com>
2024-10-29 18:21:49 +0100

    Image loading works with OpenCV

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cudaproject
 Edge detection with CUDA
+
+```bash
+mkdir build; cd build
+cmake -DOpenCV_DIR="${OpenCV_DIR}/cmake/opencv4" ..
+cmake --build .
+```
--- src/main.cu
+++ src/main.cu
@@ -1,14 +1,107 @@
+/* Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of NVIDIA CORPORATION nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 // std
 #include <iostream>
 #include <string>
 
-#include "helper_cuda.h"
+// opencv
+#include <opencv2/imgcodecs.hpp>
 
-int main(int argc, char** argv) {
+#include <npp.h>
+#include <nppi.h>
+
+#include "helper_cuda.h"
+#include "ImagesCPU.h"
+#include "ImagesNPP.h"
+
+void cudaDeviceInit() {
     int deviceCount;
     checkCudaErrors(cudaGetDeviceCount(&deviceCount));
 
-    std::cout << deviceCount << " devices found" << std::endl;
+    if (deviceCount == 0) {
+        std::cerr << "CUDA error: no devices supporting CUDA." << std::endl;
+        exit(-1);
+    }
+
+    int dev = 0;  // findCudaDevice(argc, argv);
+
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp, dev);
+    std::cerr << "cudaSetDevice GPU" << dev << " = " << deviceProp.name << std::endl;
+
+    checkCudaErrors(cudaSetDevice(dev));
+}
+
+bool printfNPPinfo() {
+    const NppLibraryVersion *libVer = nppGetLibVersion();
+
+    std::cout << "NPP Library Version " <<
+        libVer->major << '.' << libVer->minor << '.' << libVer->build << '\n';
+
+    int driverVersion, runtimeVersion;
+    cudaDriverGetVersion(&driverVersion);
+    cudaRuntimeGetVersion(&runtimeVersion);
+
+    std::cout << "  CUDA Driver  Version: "
+        << driverVersion / 1000 << '.' << (driverVersion % 100) / 10 << ",\n"
+        << "  CUDA Runtime Version: "
+        << runtimeVersion / 1000 << '.' << (runtimeVersion % 100) / 10 << '\n';
+
+    // Min spec is SM 1.0 devices
+    bool bVal = checkCudaCapabilities(1, 0);
+    return bVal;
+}
+
+bool loadImage(const std::string& filename, npp::ImageCPU_8u_C1& oHostSrc) {
+    cv::Mat img = cv::imread(filename, cv::IMREAD_GRAYSCALE);
+    if (img.empty()) {
+        throw std::runtime_error("Failed to load image: " + filename);
+        return false;
+    }
+
+    oHostSrc = npp::ImageCPU_8u_C1(img.cols, img.rows);
+    std::memcpy(oHostSrc.data(), img.data, img.cols * img.rows * sizeof(unsigned char));
+    return true;
+}
+
+int main(int argc, const char** argv) {
+    cudaDeviceInit();
+    if (!printfNPPinfo()) return 0;
+
+    std::string ifname("../data/3.2.25.png");
+
+    // declare a host image object for an 8-bit grayscale image
+    npp::ImageCPU_8u_C1 oHostSrc;
+    // load gray-scale image from disk
+    if (!loadImage(ifname, oHostSrc)) return 0;
+    // declare a device image and copy construct from the host image,
+    // i.e. upload host to device
+    npp::ImageNPP_8u_C1 oDeviceSrc(oHostSrc);
 
     return 0;
 }
